4ac1e0a705da65addf0a40218d91f734faa1c011
daramosn <daramosn@unal.edu.co>
2023-06-14 22:18:32 -0500

    fix: container width responsive without media querys

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import bgImageKid from '../../assets/images/kid_1.jpg';
+import bgImageKid from '../../assets/images/kid_1.webp';
 
 import classes from './Hero.module.scss';
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import Hero from '../components/Hero/Hero';
-import Services from '../components/Services/Services';
-import AboutMe from '../components/AboutMe/AboutMe';
-import Features from '../components/Features/Features';
-import Appointments from '../components/Appointments/Appointments';
+// import Hero from '../components/Hero/Hero';
+// import Services from '../components/Services/Services';
+// import AboutMe from '../components/AboutMe/AboutMe';
+// import Features from '../components/Features/Features';
+// import Appointments from '../components/Appointments/Appointments';
 
 const Home = () => {
 	return (
 		<>
-			<Hero />
-			<Services />
-			<AboutMe />
-			<Features />
-			<Appointments />
+			{/* <Hero /> */}
+			{/* <Services /> */}
+			{/* <AboutMe /> */}
+			{/* <Features /> */}
+			{/* <Appointments /> */}
 		</>
 	);
 };
