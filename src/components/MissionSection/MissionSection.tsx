99156615ac13915032bcb033550cd47eee723615
daramosn <daramosn@unal.edu.co>
2023-06-19 21:03:50 -0500

    feature: mission and vision section added The style is still missing

---
+++ src/components/MissionSection/MissionSection.tsx
@@ -0,0 +1,44 @@
+import { ReactComponent as HappyHeadIcon } from '../../assets/icons/happy.svg';
+import { ReactComponent as PlantIcon } from '../../assets/icons/planting.svg';
+import takingNotePsy from '../../assets/images/mission.webp';
+import classes from './MissionSection.module.scss';
+
+const MissionSection = () => {
+	return (
+		<section className={classes['mission']}>
+			<div className={classes['mission__info']}>
+				<h4 className={classes['mission__title']}>
+					Psychology specialist
+				</h4>
+				<h2 className={classes['mission__name']}>Dr. John Doe</h2>
+				<p className={classes['mission__description']}>
+					Sample text sample text sample text sample text sample text
+					sample text sample text
+				</p>
+				<div className={classes['mission__info-card']}>
+					<HappyHeadIcon
+						className={classes['mission__info-card-icon']}
+					/>
+					<div className={classes['mission__info-card-text']}>
+						<h3>Mission</h3>
+						<p>sample text sample text sample text sample text</p>
+					</div>
+				</div>
+				<div className={classes['mission__info-card']}>
+					<PlantIcon className={classes['mission__info-card-icon']} />
+					<div className={classes['mission__info-card-text']}>
+						<h3>Vision</h3>
+						<p>sample text sample text sample text sample text</p>
+					</div>
+				</div>
+			</div>
+			<img
+				className={classes['mission__figure']}
+				src={takingNotePsy}
+				alt="notes-psychologist"
+			/>
+		</section>
+	);
+};
+
+export default MissionSection;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import Services from '../components/Services/Services';
 import AboutMe from '../components/AboutMe/AboutMe';
 import Features from '../components/Features/Features';
 import Appointments from '../components/Appointments/Appointments';
+import MissionSection from '../components/MissionSection/MissionSection';
 
 const Home = () => {
 	return (
@@ -13,6 +14,7 @@ const Home = () => {
 			<AboutMe />
 			<Features />
 			<Appointments />
+			<MissionSection />
 		</>
 	);
 };
